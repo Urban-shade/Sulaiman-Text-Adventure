29bbc79c7edd3233cfb6bddf9420b146bd4aff32
MUKILTEO\1611356 <1611356@mukilteo.wednet.edu>
2024-10-09 13:37:39 -0700

    i finished a few more endings for the upstairs and the dining room. i also started making a secret ending.

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -1,4 +1,6 @@
-﻿//print backstory for the user
+﻿using System.Runtime.InteropServices;
+//print backstory for the user
+
 Console.WriteLine("Welcome to the Haunted Mansion!");
 Console.WriteLine("Your rich grandpa has recently passed away and he has left you with a lot of money and one of his Mansions.");
 Console.WriteLine("now that you are the newfound owner, you decide to take a look inside.");
@@ -6,6 +8,7 @@ Console.WriteLine("the house is dated, creaky and falling appart.");
 Console.WriteLine("You walk in the front door.");
 Console.WriteLine("You take a look around and notice 3 ways to go.");
 Console.WriteLine("where will you go? The living room dining room or upstairs?");
+Console.WriteLine("or will you ???");
 
 //promt the user for a choice
 Console.Write(">");
@@ -28,21 +31,53 @@ if (roomChoice == "living room")
     }
     else if (livingRoomChoice == "explore")
     {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
         Console.WriteLine(" you move the the center of the room and the floor opens up and you fall through.");
         Console.WriteLine("you quickly hit the ground and die.");
     }
     else if (livingRoomChoice == "wait")
     {
         Console.WriteLine("you sit down and wait for about 10 minutes till a monster appears.");
+        Console.ForegroundColor = ConsoleColor.DarkRed;
         Console.WriteLine(" the monster picks you up and rips you in two.");
     }
 
 }
 else if (roomChoice == "dining room")
 {
+    Console.WriteLine("you start walking twards the dining room...");
+    Console.WriteLine("as you get near the room you can hear the sound of people talking");
+    Console.WriteLine("will you enter or leave?");
+    Console.Write('>');
+    string diningRoomChoice = Console.ReadLine();
 
+    if (diningRoomChoice == "enter")
+    {
+        Console.WriteLine("as you walk in you see a bunch of ghosts");
+        Console.WriteLine("they turn to look at you");
+        Console.WriteLine("as you sit down to eat the food, you notice a faint cry for help comming from the food.");
+        Console.WriteLine(" you are struck with emmence fear.");
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine(" when you regain your senses you are already already one of the many plates of food on the table.");
+        Console.WriteLine(" you were eaten");
+    }
+    else if(diningRoomChoice == "leave")
+    {
+        Console.WriteLine(" you try to leave but a long clawed hand grobs you and slams you on the floor.");
+        Console.WriteLine(" you get knocked out.");
+        Console.WriteLine("as you wake up you find yourself tied to the dinner table beion locked at by a bunch of monsters.");
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("before you know it they start riping off your body parts and eating you alive.");
+    }
 }
 else if (roomChoice == "upstairs")
+{
+    Console.WriteLine("you attempt to go upstairs...");
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine(" but the floor collapses and you fall into an inescapable pit");
+    Console.WriteLine(" you wait for a few days till you invetibly parish due to lack of food and water.");
+}
+else if (roomChoice == "BURN")
 {
 
 }
